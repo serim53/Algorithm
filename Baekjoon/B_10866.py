d02ace75aca52d68a2ca43686a0e7c6eda259214
serim53 <tpfla9953@naver.com>
2022-01-23 13:57:39 +0900

    10866 : 덱

---
+++ Baekjoon/B_10866.py
@@ -0,0 +1,52 @@
+# 덱(Deque)
+# push_front X: 정수 X를 덱의 앞에 넣는다.
+# push_back X: 정수 X를 덱의 뒤에 넣는다.
+# pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 덱에 들어있는 정수의 개수를 출력한다.
+# empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+# front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+
+
+import sys
+
+input = sys.stdin.readline
+
+deque = []
+
+n = int(input())
+for i in range(n):
+    command = input().split()
+    if command[0] == 'push_front':
+        deque.insert(0, command[1])
+    elif command[0] == 'push_back':
+        deque.append(command[1])
+    elif command[0] == 'pop_front':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.pop(0))
+    elif command[0] == 'pop_back':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.pop())
+    elif command[0] == 'size':
+        print(len(deque))
+    elif command[0] == 'empty':
+        if len(deque) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[0])
+    elif command[0] == 'back':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[-1])
