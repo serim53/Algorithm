7a901fc3da68115795d647bd24b235e466f323d8
serim53 <tpfla9953@naver.com>
2022-01-25 21:32:20 +0900

    9613 : GCD 합

---
+++ Baekjoon/B_9613.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+for _ in range(t):
+    arr = list(map(int, input().split()))
+    k = arr.pop(0)
+    sum = 0
+    for i in range(k):
+        for j in range(k):
+            if i < j:
+                sum += gcd(arr[i], arr[j])
+    print(sum)
