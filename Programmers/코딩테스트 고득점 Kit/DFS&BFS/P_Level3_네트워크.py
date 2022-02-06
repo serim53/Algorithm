0672ac297b62ad8442712fbeb9344d6d12e50ebc
serim53 <tpfla9953@naver.com>
2022-02-06 19:36:21 +0900

    Level3 : 네트워크

--- Programmers/코딩테스트 고득점 Kit/DFS&BFS/P_Level3_네트워크.py
+++ Programmers/코딩테스트 고득점 Kit/DFS&BFS/P_Level3_네트워크.py
@@ -1,18 +1,16 @@
 def solution(n, computers):
     answer = 0
-    networks = [[] for _ in range(n)]
-    for i in range(1, n):
-        if computers[0][i] == 1:
-            networks[0].append(i)
-
-    idx = 0
-    for _ in networks[idx]:
-        for j in range(idx + 1, n):
-            if computers[idx][j] == 1:
-                networks[idx].append(j)
-        idx += 1
-
-    return networks
+    visited = [False for _ in range(n)]
+    for com in range(n):
+        if not visited[com]:
+            DFS(n, computers, com, visited)
+            answer += 1
+    return answer
 
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+def DFS(n, computers, com, visited):
+    visited[com] = True
+    for connect in range(n):
+        if connect != com and computers[com][connect] == 1:
+            if not visited[connect]:
+                DFS(n, computers, connect, visited)
