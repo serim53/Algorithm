156c4c9d932ec23cf95269b212d3b3f2025721b1
serim53 <tpfla9953@naver.com>
2022-02-10 22:39:49 +0900

    Level2 : 프린터

---
+++ Programmers/코딩테스트 고득점 Kit/스택&큐/P_Level2_프린터.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    queue = deque([(v, i) for i, v in enumerate(priorities)])
+
+    while len(queue):
+        item = queue.popleft()
+        if queue and max(queue)[0] > item[0]:
+            queue.append(item)
+        else:
+            answer += 1
+            if item[1] == location:
+                break
+
+    return answer
