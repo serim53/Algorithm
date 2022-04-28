44d7666f91e9f6a0ce036e9e2b9236fbf50a3c6f
serim53 <tpfla9953@naver.com>
2022-04-28 21:05:04 +0900

    20056 : 마법사 상어와 파이어볼

--- Baekjoon/삼성 SW 역량 테스트 기출 문제/B_20056.py
+++ Baekjoon/삼성 SW 역량 테스트 기출 문제/B_20056.py
@@ -1,37 +1,42 @@
 n, m, k = map(int, input().split())
+balls = []
 # r, c, m, s, d
-balls = [list(map(int, input().split())) for _ in range(m)]
+# balls = [list(map(int, input().split())) for _ in range(m)]
+for _ in range(m):
+    _r, _c, _m, _s, _d = list(map(int, input().split()))
+    balls.append([_r-1, _c-1, _m, _s, _d])
 graph = [[[] for _ in range(n)] for _ in range(n)]
 # 행 x, 열 y
-direction = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
 
-result =0
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
 for _ in range(k):
     while balls:
         cr, cc, cm, cs, cd = balls.pop(0)
-        nr = (cr + direction[cd][0] * cs) % n
-        nc = (cc + direction[cd][1] * cs) % n
+        nr = (cr + cs * dx[cd]) % n  # 1번-N번 행 연결되어있기 때문
+        nc = (cc + cs * dy[cd]) % n
         graph[nr][nc].append([cm, cs, cd])
 
     for i in range(n):
         for j in range(n):
             if len(graph[i][j]) >= 2:
                 mass, speed, cnt_odd, cnt_even, cnt = 0, 0, 0, 0, len(graph[i][j])
-                for b in graph[i][j]:
-                    mass += b[0]
-                    speed += b[1]
-                    if b[2] % 2:
+                while graph[i][j]:
+                    _m, _s, _d = graph[i][j].pop(0)
+                    mass += _m
+                    speed += _s
+                    if _d % 2:
                         cnt_odd += 1
                     else:
                         cnt_even += 1
-                if cnt_odd == cnt_even == cnt:
+                if cnt_odd == cnt or cnt_even == cnt:
                     d = [0, 2, 4, 6]
                 else:
                     d = [1, 3, 5, 7]
                 if mass // 5:
-                    for _d in d:
-                        balls.append([i, j, mass//5, speed/len(graph[i][j]), _d])
+                    for dd in d:
+                        balls.append([i, j, mass//5, speed//cnt, dd])
 
             if len(graph[i][j]) == 1:
                 balls.append([i, j] + graph[i][j].pop())
