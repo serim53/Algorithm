a5f5f182994026b3463f7e50cd68961dda1c6a82
serim53 <tpfla9953@naver.com>
2022-07-17 15:23:24 +0900

    [SWEA] 1959 : 두 개의 문자열

--- SWEA/1959-두 개의 숫자열.py
+++ SWEA/1959-두 개의 숫자열.py
@@ -4,22 +4,21 @@ for test_case in range(1, t + 1):
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
 
-    if n == m:
-        max_value = 0
-        for i in range(n):
-            max_value += a[i] * b[i]
-    else:
-        if n > m:
-            sums = [0] * (n - m + 1)
-            for i in range(len(sums)):
-                for j in range(-1, -m - 1, -1):
-                    sums[i] += a[j - i] * b[j]
-        else:
-            sums = [0] * (m - n + 1)
-            for i in range(len(sums)):
-                for j in range(-1, -n - 1, -1):
-                    sums[i] += a[j] * b[j - i]
-        sums.sort()
-        max_value = sums[-1]
+    max_value = 0
 
-    print("#{} {}".format(t, max_value))
+    if n > m:
+        for i in range(n - m + 1):
+            result = 0
+            for j in range(m):
+                result += b[j] * a[i + j]
+            if result > max_value:
+                max_value = result
+    else:
+        for i in range(m - n + 1):
+            result = 0
+            for j in range(n):
+                result += a[j] * b[i + j]
+            if result > max_value:
+                max_value = result
+
+    print("#{} {}".format(test_case, max_value))
