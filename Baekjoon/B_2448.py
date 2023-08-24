4dc54179d4d846136779b3610b3887268da3317d
serim <tpfla9953@naver.com>
2023-08-24 13:55:46 +0900

    2448 : 별 찍기 - 11

---
+++ Baekjoon/B_2448.py
@@ -0,0 +1,21 @@
+n = int(input())
+stars = [[' '] * 2 * n for _ in range(n)]
+
+
+def recursion(i, j, size):
+    if size == 3:
+        stars[i][j] = '*'
+        stars[i + 1][j - 1] = stars[i + 1][j + 1] = '*'
+        for k in range(-2, 3):
+            stars[i + 2][j - k] = '*'
+
+    else:
+        temp = size // 2
+        recursion(i, j, temp)
+        recursion(i + temp, j - temp, temp)
+        recursion(i + temp, j + temp, temp)
+
+
+recursion(0, n - 1, n)
+for star in stars:
+    print("".join(star))
