75f0aec64b57df40154636c83985e92a3131c4e3
serim53 <tpfla9953@naver.com>
2024-05-21 23:55:00 +0900

    131127 : 할인 행사

---
+++ Programmers/연습문제_LEVEL2/131127_할인 행사.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    answer = 0
+    dict = {}
+    for i in range(len(want)):
+        dict[want[i]] = number[i]
+    for i in range(len(discount) - 9):
+        cnt = Counter(discount[i:i + 10])
+        if cnt == dict:
+            answer += 1
+    return answer
