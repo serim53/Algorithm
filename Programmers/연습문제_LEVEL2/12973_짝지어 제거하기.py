a404ecdc840daf8a3374fd5030e5293d9d450248
serim53 <tpfla9953@naver.com>
2024-06-02 23:09:44 +0900

    12973 : 짝지어 제거하기

--- Programmers/연습문제_LEVEL2/12973_짝지어 제거하기.py
+++ Programmers/연습문제_LEVEL2/12973_짝지어 제거하기.py
@@ -1,6 +1,13 @@
 def solution(s):
-    answer = -1
-
-    
-
-    return answer
+    stack = []
+    for i in range(len(s)):
+        if stack and stack[-1] == s[i]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+        if len(stack) >= 2 and stack[-2] == stack[-1]:
+            stack = stack[:-2]
+    if stack:
+        return 0
+    else:
+        return 1
